68bb1f58e0a802d99126011bda9b5224ea989e28
Smkale2232 <Smkale2232@gmail.com>
2024-07-11 23:19:07 +0530

    v3

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello from Go")
+
+	// storing the hexadecimal
+	// values in variables
+	x := 0xFF
+	y := 0x9C
+
+	// Displaying the values
+	fmt.Printf("Type of variable x is %T\n", x)
+	fmt.Printf("Value of x in hexadecimal is %X\n", x)
+	fmt.Printf("Value of x in decimal is %v\n", x)
+
+	fmt.Printf("Type of variable y is %T\n", y)
+	fmt.Printf("Value of y in hexadecimal is %X\n", y)
+	fmt.Printf("Value of y in decimal is %v\n", y)
+
 }
